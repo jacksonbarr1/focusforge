7f47c33d0319fc8a377bef3dd4db63eaf910c215
Jackson Barr <jacksonbarr2021@gmail.com>
2025-03-17 13:00:19 -0400

    Create TitleScreen component

--- client/src/app/routes/startup/Startup.jsx
+++ client/src/app/routes/startup/Startup.jsx
@@ -1,9 +1,17 @@
 // contains title screen
-import React from 'react'
+import React from "react";
+import { Button, Col } from "nes-ui-react";
+import TitleScreen from "../../../components/titlescreen/TitleScreen.jsx";
+import "./startup.css";
 
 export const StartupPage = () => {
   return (
-    <div>StartupPage</div>
+    <div className="startup-container">
+        <TitleScreen />
+        <Button color="primary" borderInverted>Sign in</Button>
+        <Button color="primary" borderInverted>Sign up</Button>
+    </div>
+
   )
 }
 
--- client/src/components/titlescreen/TitleScreen.jsx
+++ client/src/components/titlescreen/TitleScreen.jsx
@@ -1,42 +1,11 @@
-import { useState } from "react";
-import { Input } from "nes-ui-react";
-import { supabase } from "../../config/supabaseClient";
+import { Container, Heading } from "nes-ui-react";
+import "./titlescreen.css";
 
 const TitleScreen = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const { data, error } = await supabase.auth.signUp(
-            {
-                email: email,
-                password: password
-            }
-        )
-        console.log("Sign Up Data: ", data);
-        console.log("Sign Up Error: ", error);
-    };
-
     return (
-        <>
-            <div>TitleScreen</div>
-            <form onSubmit={handleSubmit}>
-                <Input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e)}
-                />
-                <Input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e)}
-                />
-                <button type="submit">Submit</button>
-            </form>
-        </>
+        <Container className="title-container">
+            <Heading centered="true">Focus Forge</Heading>
+        </Container>
     );
 };
 
